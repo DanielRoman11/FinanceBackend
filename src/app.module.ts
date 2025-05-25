0d7c3447a2b768902316472b8389c9fe7a24041c
Daniel Román <danielroman.05111@gmail.com>
2025-05-25 02:30:22 -0500

    feat: AppModule, using Joi to check the env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
 import { PaymentPlanModule } from './payment-plan/payment-plan.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -18,6 +19,17 @@ import { PaymentPlanModule } from './payment-plan/payment-plan.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
+      validationSchema: Joi.object({
+        //? -- API --
+        DATABASE_URL: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+        SESSION_SECRET: Joi.string().required(),
+        BACKEND_URL: Joi.string().required(),
+        //? -- GOOGLE OAUTH --
+        GOOGLE_CLIENT_ID: Joi.string().required(),
+        GOOGLE_SECRET_ID: Joi.string().required(),
+        GOOGLE_REDIRECT_URL: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
