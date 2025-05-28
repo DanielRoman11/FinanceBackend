1faa3e055e4390057304fd446cfc6b432768d1fa
Daniel Román <danielroman.05111@gmail.com>
2025-05-28 18:30:31 -0500

    feat: PaymentPlanService, new methods

--- src/payment-plan/payment-plan.service.spec.ts
+++ src/payment-plan/payment-plan.service.spec.ts
@@ -6,11 +6,20 @@ import { Currency, PaymentPlanStatus, PaymentInterval } from '../utils/enums';
 import { CreatePaymentPlanDto } from './dto/create-payment-plan.dto';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { UpdatePaymentPlanDto } from './dto/update-payment-plan.dto';
+import {
+  BadRequestException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserService } from '../auth/user.service';
 
 describe('PaymentPlanService', () => {
   let service: PaymentPlanService;
+  let userService: UserService;
   let repo: Repository<PaymentPlan>;
   let mockUser: User;
+  let otherUser: User;
   let mockResult: PaymentPlan;
 
   beforeEach(async () => {
@@ -21,15 +30,32 @@ describe('PaymentPlanService', () => {
           provide: getRepositoryToken(PaymentPlan),
           useValue: {
             save: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: UserService,
+          useValue: {
+            findUsersByIds: jest.fn(),
           },
         },
       ],
     }).compile();
 
     service = module.get<PaymentPlanService>(PaymentPlanService);
+    userService = module.get<UserService>(UserService);
     repo = module.get<Repository<PaymentPlan>>(getRepositoryToken(PaymentPlan));
     mockUser = {
       id: 'afdb2c8d-dc32-4742-b8d1-2435110dde52',
+      username: 'test-user',
+      email: 'test@test.com',
+      picture: 'test-picture',
+      createdAt: new Date(),
+    };
+    otherUser = {
+      id: 'otherId',
       username: 'test-user',
       email: 'test@test.com',
       picture: 'test-picture',
@@ -42,7 +68,7 @@ describe('PaymentPlanService', () => {
       createdAt: new Date(),
       owner: mockUser,
       status: PaymentPlanStatus.ACTIVE,
-      collaborators: [],
+      collaborators: [otherUser],
       startDate: new Date(),
       endDate: new Date(),
       currency: Currency.COP,
@@ -59,14 +85,17 @@ describe('PaymentPlanService', () => {
     it('should return a new instance of PaymentPlan', async () => {
       const dto: CreatePaymentPlanDto = {
         name: 'Test',
+        ownerId: mockUser.id,
         totalAmount: 10000000,
         currency: Currency.COP,
         interval: PaymentInterval.WEEKLY,
         status: PaymentPlanStatus.ACTIVE,
         startDate: '2025-05-11T00:00:00.000Z',
         endDate: '2025-05-11T00:00:00.000Z',
-        participantIds: [],
+        participantIds: [otherUser.id],
       };
+
+      jest.spyOn(repo, 'save').mockResolvedValue(mockResult);
 
       const result = await service.create(dto, mockUser);
 
@@ -75,13 +104,125 @@ describe('PaymentPlanService', () => {
     });
   });
 
-  describe('getAll', () => {
+  describe('findAll', () => {
     it('should return all instances of PaymentPlan', async () => {
       const mockResultArray: PaymentPlan[] = [mockResult];
+
+      jest.spyOn(repo, 'find').mockResolvedValue(mockResultArray);
+
       const result = await service.findAll(mockUser);
 
-      expect(result).toEqual([mockResultArray]);
+      expect(result).toEqual(mockResultArray);
       expect(repo.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a payment plan', async () => {
+      jest.spyOn(repo, 'findOne').mockResolvedValue(mockResult);
+
+      const result = await service.findOne('1', mockUser);
+
+      expect(result).toEqual(mockResult);
+      expect(repo.findOne).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an error if the payment plan does not exist', async () => {
+      jest.spyOn(repo, 'findOne').mockResolvedValue(undefined);
+
+      await expect(service.findOne('1', mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+      await expect(service.findOne('1', mockUser)).rejects.toThrow(
+        'Payment plan with id 1 not found',
+      );
+    });
+  });
+
+  describe('Update', () => {
+    let dto: UpdatePaymentPlanDto;
+    let mockInitial: PaymentPlan;
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+      dto = {
+        name: 'Test',
+        totalAmount: 10000000,
+        currency: Currency.COP,
+        interval: PaymentInterval.WEEKLY,
+        status: PaymentPlanStatus.ACTIVE,
+        startDate: '2025-05-11T00:00:00.000Z',
+        endDate: '2025-05-11T00:00:00.000Z',
+        participantIds: [otherUser.id],
+      };
+      mockInitial = {
+        id: mockResult.id,
+        createdAt: new Date(),
+        owner: mockUser,
+        name: 'Before',
+        totalAmount: 10000000,
+        currency: Currency.COP,
+        interval: PaymentInterval.WEEKLY,
+        status: PaymentPlanStatus.ACTIVE,
+        startDate: new Date(),
+        endDate: new Date(),
+        collaborators: [],
+        paymentRecords: [],
+      };
+    });
+
+    it('should update a payment plan', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockInitial);
+      jest.spyOn(userService, 'findUsersByIds').mockResolvedValue([otherUser]);
+      jest.spyOn(repo, 'save').mockResolvedValue(mockResult);
+
+      const result = await service.update(mockResult.id, dto, mockUser);
+
+      expect(result).toEqual(mockResult);
+      expect(userService.findUsersByIds).toHaveBeenCalledTimes(1);
+      expect(service.findOne).toHaveBeenCalledTimes(1);
+      expect(repo.save).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception if the Payment Plan is not found', async () => {
+      jest
+        .spyOn(service, 'findOne')
+        .mockRejectedValue(
+          new NotFoundException('Payment plan with id 1 not found'),
+        );
+      await expect(service.update('2', dto, mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+
+      expect(service.findOne).toHaveBeenCalledTimes(1);
+      expect(repo.save).toHaveBeenCalledTimes(0);
+    });
+
+    it('should throw an exception if payment plan is not owned by the user', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockInitial);
+
+      await expect(service.update('1', dto, otherUser)).rejects.toThrow(
+        ForbiddenException,
+      );
+      expect(service.findOne).toHaveBeenCalledTimes(1);
+      expect(repo.save).toHaveBeenCalledTimes(0);
+    });
+
+    it('should throw an exception if some participants do not exist', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockInitial);
+      jest
+        .spyOn(userService, 'findUsersByIds')
+        .mockResolvedValue([mockUser, otherUser]);
+
+      const error: BadRequestException = await service
+        .update('1', dto, mockUser)
+        .catch((err) => err);
+
+				expect(service.findOne).toHaveBeenCalledTimes(1);
+				expect(userService.findUsersByIds).toHaveBeenCalledTimes(1);
+				expect(error).toBeInstanceOf(BadRequestException);
+				expect(error.message).toBe('Some participants do not exist');
+      expect(repo.save).toHaveBeenCalledTimes(0);
     });
   });
 });
--- src/payment-plan/payment-plan.service.ts
+++ src/payment-plan/payment-plan.service.ts
@@ -1,42 +1,73 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePaymentPlanDto } from './dto/create-payment-plan.dto';
 import { UpdatePaymentPlanDto } from './dto/update-payment-plan.dto';
 import { PaymentPlan } from './entities/payment-plan.entity';
 import { User } from '../auth/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UserService } from '../auth/user.service';
 
 @Injectable()
 export class PaymentPlanService {
   constructor(
     @InjectRepository(PaymentPlan)
     private paymentPlanRepo: Repository<PaymentPlan>,
+    private userService: UserService,
   ) {}
 
   async create(
     dto: CreatePaymentPlanDto,
     currentUser: User,
   ): Promise<PaymentPlan> {
-    try {
-      return await this.paymentPlanRepo.save({ ...dto, owner: currentUser });
-    } catch (err) {
-      console.error(err);
-    }
+    return await this.paymentPlanRepo.save({ ...dto, owner: currentUser });
   }
 
   async findAll(user: User): Promise<PaymentPlan[]> {
-    throw new NotImplementedException();
+    return this.paymentPlanRepo.find({ where: { owner: { id: user.id } } });
   }
 
-  async findOne(id: string): Promise<PaymentPlan> {
-    throw new NotImplementedException();
+  async findOne(id: string, user: User): Promise<PaymentPlan> {
+    const paymentPlan = await this.paymentPlanRepo.findOne({
+      where: { owner: { id: user.id }, id },
+    });
+    if (!paymentPlan)
+      throw new NotFoundException(`Payment plan with id ${id} not found`);
+    return paymentPlan;
   }
 
-  async update(id: string, dto: UpdatePaymentPlanDto): Promise<PaymentPlan> {
-    throw new NotImplementedException();
+  async update(
+    id: string,
+    dto: UpdatePaymentPlanDto,
+    user: User,
+  ): Promise<PaymentPlan> {
+    const paymentPlan = await this.findOne(id, user);
+    if (paymentPlan.owner.id !== user.id)
+      throw new ForbiddenException(
+        'You are not authorized to update this payment plan',
+      );
+    const collaborators = await this.userService.findUsersByIds(
+      dto.participantIds,
+    );
+    if (collaborators.length !== dto.participantIds.length)
+      throw new BadRequestException('Some participants do not exist');
+    return await this.paymentPlanRepo.save({
+      ...paymentPlan,
+      ...dto,
+      collaborators,
+    });
   }
 
-  async remove(id: string) {
-    throw new NotImplementedException();
+  async remove(id: string, user: User) {
+    const paymentPlan = await this.findOne(id, user);
+    if (paymentPlan.owner.id !== user.id)
+      throw new ForbiddenException(
+        'You are not authorized to delete this payment plan',
+      );
+    return await this.paymentPlanRepo.delete(id);
   }
 }
