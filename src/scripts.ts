954cd3e81d1ee627996550d3ff213c2fe3fa92e5
Daniel Román <danielroman.05111@gmail.com>
2025-05-28 11:12:18 -0500

    fix: scripts.ts, showing errors, warnings, logs and debugs in the scripts console

--- src/scripts.ts
+++ src/scripts.ts
@@ -2,6 +2,6 @@ import { AppModule } from './app.module';
 import { CommandFactory } from 'nest-commander';
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule);
+  await CommandFactory.run(AppModule, ["error", "warn", "log", "debug"]);
 }
 bootstrap();
