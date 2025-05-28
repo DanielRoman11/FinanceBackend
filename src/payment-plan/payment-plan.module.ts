b5bc16ddaa88038431cf8b2fe7cc4351e0391337
Daniel Román <danielroman.05111@gmail.com>
2025-05-28 18:28:37 -0500

    feat: PaymentPlanModule, importing AuthModule, to use UserService

--- src/payment-plan/payment-plan.module.ts
+++ src/payment-plan/payment-plan.module.ts
@@ -6,9 +6,10 @@ import { PaymentPlan } from './entities/payment-plan.entity';
 import { PaymentRecord } from './entities/payment-record.entity';
 import { PaymentRecordController } from './payment-record.controller';
 import { PaymentRecordService } from './payment-record.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaymentPlan, PaymentRecord])],
+  imports: [TypeOrmModule.forFeature([PaymentPlan, PaymentRecord]), AuthModule],
   controllers: [PaymentPlanController, PaymentRecordController],
   providers: [PaymentPlanService, PaymentRecordService],
 })
